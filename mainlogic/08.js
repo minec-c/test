710ca10204f3e03d745a0c8eca2fb01095575192
minec-c <reciprocalidea@gmail.com>
2025-05-20 17:02:58 -0400

    Update 08.js

--- mainlogic/08.js
+++ mainlogic/08.js
@@ -3,7 +3,7 @@ import {scene} from '../mainlogic/04.js';
 // Floor
 const floorMesh = new THREE.Mesh(
 	new THREE.PlaneGeometry(50, 50),
-	new THREE.MeshLambertMaterial({ color: 'burlywood' })
+	new THREE.MeshLambertMaterial({ color: 'forestgreen' })
 );
 floorMesh.rotation.x = -Math.PI / 2;
 scene.add(floorMesh);
