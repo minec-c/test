3d85e71885af56c119dfabaf055c156bca6f529a
minec-c <reciprocalidea@gmail.com>
2025-05-20 16:19:36 -0400

    Update 010.js

--- mainlogic/010.js
+++ mainlogic/010.js
@@ -2,10 +2,12 @@ import * as THREE from 'three';
 import {renderer} from '../mainlogic/02.js';
 import {scene} from '../mainlogic/04.js';
 import {camera} from '../mainlogic/05.js';
+import {mixer} from '../mainlogic/0.9.js';
 // Animation
 const clock = new THREE.Clock();
 function draw() {
 	const delta = clock.getDelta();
+	if (mixer) mixer.update(delta);
 	renderer.render(scene, camera);
 	//rederer.setAnimationLoop(draw); ////SILENT BUG: BLACK SCREEN AND ZERO PUSH ERRORS
 }
