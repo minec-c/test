f4e8555b267f3f3aa7784727cb814022c54bc5de
minec-c <reciprocalidea@gmail.com>
2025-05-19 21:34:06 -0400

    Create 08.js

---
+++ mainlogic/08.js
@@ -0,0 +1,8 @@
+// Floor
+const floorMesh = new THREE.Mesh(
+	new THREE.PlaneGeometry(50, 50),
+	new THREE.MeshLambertMaterial({ color: 'burlywood' })
+);
+floorMesh.rotation.x = -Math.PI / 2;
+scene.add(floorMesh);
+
