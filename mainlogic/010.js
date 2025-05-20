61124905581c7c2487c8fcf338707480be3b5d2c
minec-c <reciprocalidea@gmail.com>
2025-05-20 00:05:57 -0400

    Update 010.js

--- mainlogic/010.js
+++ mainlogic/010.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {renderer} from '../mainlogic/02.js';
+import {scene} from '../mainlogic/04.js';
 // Animation
 const clock = new THREE.Clock();
 function draw() {
