538d33449a5cf3600e5baec336c9ef2bf1b0332c
minec-c <reciprocalidea@gmail.com>
2025-05-19 21:33:44 -0400

    Create 06.js

---
+++ mainlogic/06.js
@@ -0,0 +1,9 @@
+
+// Light
+const ambientLight = new THREE.AmbientLight('white', 1);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight('white', 0.5);
+directionalLight.position.set(1, 1, 2);
+scene.add(directionalLight);
+
