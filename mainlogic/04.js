ee27ec01c0b54ed6af6796202805b2f0560962ed
minec-c <reciprocalidea@gmail.com>
2025-05-20 17:17:05 -0400

    Update 04.js

--- mainlogic/04.js
+++ mainlogic/04.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('white');
+//scene.background = new THREE.Color('white');
+scene.background = new THREE.Color('skyblue');
 export {scene};
