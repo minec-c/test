e20b99627c27a813430477bd2622676621282075
minec-c <reciprocalidea@gmail.com>
2025-05-20 18:36:26 -0400

    Update 04.js

--- mainlogic/04.js
+++ mainlogic/04.js
@@ -8,6 +8,7 @@ const scene = new THREE.Scene();
 const texloader = new THREE.TextureLoader();
 texloader.load(
   '../texture/bgtexbluesky.png',
+  '../texture/treetest01bbnoanim.png',
   function (texture){  scene.background = texture;},
   undefined,
   function(err){ console.log('Error loading background texture:',err);}
