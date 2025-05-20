ed742eb57834ec939cac6db303796ffab5cee0e2
minec-c <reciprocalidea@gmail.com>
2025-05-20 00:22:15 -0400

    Update 011.js

--- mainlogic/011.js
+++ mainlogic/011.js
@@ -1,4 +1,5 @@
 import {camera} from '../mainlogic/05.js';
+import {renderer} from '../mainlogic/02.js';
 // Responsive
 function setSize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
