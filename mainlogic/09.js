18cef9b5a255f255666c66f83b6117bb8861e730
minec-c <reciprocalidea@gmail.com>
2025-05-20 13:42:08 -0400

    Update 09.js

--- mainlogic/09.js
+++ mainlogic/09.js
@@ -4,13 +4,14 @@ import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {renderer} from '../mainlogic/02.js';
 import {scene} from '../mainlogic/04.js';
 // GLTF Loader
+let mixer
 const gltfLoader = new GLTFLoader();
 gltfLoader.load(
 	'../gltf/texture01dim256px.glb',
 	function (gltf){				//	(gltf) => {
 		const model = gltf.scene;
 		scene.add(model);
-		const mixer = new THERR.AnimationMixer(model);
+		mixer = new THERR.AnimationMixer(model);
 		const clips = gltf.animations;
 		const clip = THREE.AnimationClip.findByName(clips, 'CubeAction');
 		const action = mixer.clipAction(clip);
