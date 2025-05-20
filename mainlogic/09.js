4b6fc31a4adfb9064ef14f6b06d5bfe68fe67fd9
minec-c <reciprocalidea@gmail.com>
2025-05-19 21:34:18 -0400

    Create 09.js

---
+++ mainlogic/09.js
@@ -0,0 +1,14 @@
+// GLTF Loader
+const gltfLoader = new GLTFLoader();
+gltfLoader.load(
+	'./texture01dim256px.glb',
+	(gltf) => {
+		const model = gltf.scene;
+		scene.add(model);
+	},
+	undefined,
+	(error) => {
+		console.error('An error happened loading the GLTF model:', error);
+	}
+);
+
