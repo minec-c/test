e7be4af2c54ba7a41ee71243bdee9eaac20ad30c
minec-c <reciprocalidea@gmail.com>
2025-05-19 23:24:16 -0400

    Update 07.js

--- mainlogic/07.js
+++ mainlogic/07.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {scene} from '../mainlogic/04.js';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
