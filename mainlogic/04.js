f42bfe4da2ba72585c20792955da942028b87804
minec-c <reciprocalidea@gmail.com>
2025-05-20 17:43:42 -0400

    Update 04.js

--- mainlogic/04.js
+++ mainlogic/04.js
@@ -2,5 +2,15 @@ import * as THREE from 'three';
 // Scene
 const scene = new THREE.Scene();
 //scene.background = new THREE.Color('white');
-scene.background = new THREE.Color('skyblue');
+//scene.background = new THREE.Color('skyblue'); ////WORKS
+
+//Load Background Texture
+const texloader = new THREE.TextureLoader();
+texloader.load(
+  '../texture/bgtexbluesky.png',
+  function (texture){  scene.background = texture;},
+  undefined,
+  function(err){ console.log('Error loading background texture:',err);}
+  );
+
 export {scene};
