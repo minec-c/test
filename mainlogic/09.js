c0b944c1b8a2313cd6f070cd7ce67fc53600a61b
minec-c <reciprocalidea@gmail.com>
2025-05-22 01:54:42 -0400

    Update 09.js

--- mainlogic/09.js
+++ mainlogic/09.js
@@ -44,7 +44,7 @@ gltfLoader.load('../gltf/treetest02PBSDFnoanim.glb',
                   const plane = gltf.scene;
                   // plane.position.set(122,0,0);
                   plane.traverse(function (node){
-                  if (node.isMesh){ node.castShadow = true; node.receiveShadow = true;  planemesh = node;}});
+                  if (node.isMesh){ node.castShadow = true; node.receiveShadow = true;  }});
                   // plane.receiveShadow = true;
                   // plane.castShadow = true;
                   scene.add(plane);
@@ -54,14 +54,6 @@ gltfLoader.load('../gltf/treetest02PBSDFnoanim.glb',
                 function (error) {console.error('Error loading treetest01bbnoanim.glb:', 
                 error);
 });
-function animate(){
-  requestAnimationFrame(animate);
-    if (planemesh){
-      planemesh.lookAt(camera.position);
-    }
-    renderer.render(scene,camera);
-  }
-animate();
 function handleGLTF(gltf) {  
   if (gltf.animations.length === 0){        console.log('No animations found in the GLB file.');   } 
   else {    gltf.animations.forEach(  function (clip, index) {      console.log(`Animation ${index}:`, clip.name);    });
