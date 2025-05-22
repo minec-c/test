1deabbd33cf91850b184663b0703329bc083b5fa
minec-c <reciprocalidea@gmail.com>
2025-05-22 14:09:15 -0400

    Update 08.js

--- mainlogic/08.js
+++ mainlogic/08.js
@@ -9,7 +9,7 @@ grasstex.wrapT = THREE.RepeatWrapping;
 grasstex.repeat.set(10,10); //Adjust tiling here
 // Floor
 const floorMesh = new THREE.Mesh(
-	new THREE.PlaneGeometry(50, 50),
+	new THREE.PlaneGeometry(11, 11),
 	// new THREE.MeshLambertMaterial({ color: 'forestgreen' })
 	new THREE.MeshLambertMaterial({map:grasstex, wireframe:false}),
 	//GRID PlaneGeometry making not visible
