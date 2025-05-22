16cc526606bfea963ef7ce627309c67d443306a4
minec-c <reciprocalidea@gmail.com>
2025-05-21 20:17:00 -0400

    Update 06.js

--- mainlogic/06.js
+++ mainlogic/06.js
@@ -9,3 +9,10 @@ const directionalLight = new THREE.DirectionalLight('white', 0.5);
 directionalLight.position.set(1, 1, 2);
 scene.add(directionalLight);
 
+//DIRECTIONAL LIGHT
+const dl = new THREE.DirectionalLight(0xffffff, 1);
+dl.position.x +=20;
+dl.position.y +=20;
+dl.position.z +=20;
+dl.castShadow = true;
+scene.add(dl);
