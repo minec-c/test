337ab1a2d61ecd973dfe536a637d265832a24523
minec-c <reciprocalidea@gmail.com>
2025-05-21 20:08:42 -0400

    Update 03.js

--- mainlogic/03.js
+++ mainlogic/03.js
@@ -2,4 +2,5 @@ import * as THREE from 'three';
 import {renderer} from '../mainlogic/02.js';
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
-
+//LIGHT MAP
+renderer.shadowMap.enabled = true;
